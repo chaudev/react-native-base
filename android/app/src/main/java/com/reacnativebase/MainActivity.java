4ae1c69e873ce626b3d71100e5b508ecf78f8cb5
Châu 10 Ngón <soc900k@gmail.com>
2022-10-11 07:12:09 +0700

    Update App Name, Icon, Splash Screen

--- android/app/src/main/java/com/reacnativebase/MainActivity.java
+++ android/app/src/main/java/com/reacnativebase/MainActivity.java
@@ -1,25 +1,51 @@
 package com.reacnativebase;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
+import org.devio.rn.splashscreen.SplashScreen;
+
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
+  private void setStatusBarTransparnt() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      Window window = getWindow();
+      window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+      window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+      View decorView = window.getDecorView();
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+      } else {
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+      }
+      window.setStatusBarColor(Color.TRANSPARENT);
+    }
+  }
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+
+    setStatusBarTransparnt();
+
+    super.onCreate(savedInstanceState);
+  }
+
   @Override
   protected String getMainComponentName() {
     return "reacNativeBase";
   }
 
-  /**
-   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
-   * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
-   * (Paper).
-   */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
